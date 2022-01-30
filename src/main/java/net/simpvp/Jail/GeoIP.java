17025496e5c72754cc18d75df263b294da54d9f5
MoWobbler <64741387+MoWobbler@users.noreply.github.com>
2022-01-30 15:28:09 -0500

    Update GeoIP.java

--- src/main/java/net/simpvp/Jail/GeoIP.java
+++ src/main/java/net/simpvp/Jail/GeoIP.java
@@ -15,6 +15,7 @@ import org.bukkit.configuration.file.YamlConfiguration;
 
 import com.maxmind.geoip.Country;
 import com.maxmind.geoip.LookupService;
+import org.bukkit.entity.Player;
 
 public class GeoIP {
 
@@ -143,5 +144,23 @@ public class GeoIP {
 		}
 
 		return country.getCode();
+	}
+
+	public static boolean check_asn(Player player, InetAddress address) {
+		if (GeoIP.bad_asns == null || GeoIP.bad_asns.isEmpty()) {
+			return false;
+		}
+
+		String as = GeoIP.getAs(address);
+		Integer asn = GeoIP.getAsn(as);
+		if (asn == null) {
+			return false;
+		}
+
+		String reason = GeoIP.bad_asns.get(asn);
+		if (reason == null) {
+			return false;
+		}
+		return true;
 	}
 }
